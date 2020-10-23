e6a7c3935359b83c811bab3f09f2ff2f179bfb59
sinitsyn47 <sk8er47@bk.ru>
2020-10-23 16:07:41 +0300

    Вычисление расстояния между точками с помощью метода

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyProgramm.java
@@ -13,21 +13,14 @@ public class MyProgramm {
     Rectangle r = new Rectangle(5,7); //задаём объект
     System.out.println("Площадь промоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-  double x1 = 2;
-  double y1 = 5;
-  double x2 = 4;
-  double y2 = 15;
-    System.out.println("Расстояние между точками " + "P1 (" + x1 + "," + y1 + ") и P2 (" + x2 + "," + y2 + ") равна " + distance(x1,y1,x2,y2) );
+    Point p = new Point(2,5,9, 4);
+    System.out.println("Расстояние между точками плоскости " + "P1 (" + p.x1 + "," + p.y1 + ") и P2 (" + p.x2 + "," + p.y2 + ") равна " + p.distance() );
   }
 
   public static void hello(String word) {
     System.out.println("Hello, " + word + "!");
   }
 
-  public static double distance(double x1, double y1, double x2, double y2){
-    double sq =  ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
-    return Math.sqrt(sq);
-  }
 
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -6,4 +6,15 @@ public class Point {
   public double x2;
   public double y2;
 
+  public Point(double x1, double y1, double x2, double y2) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+  }
+
+  public double distance() {
+    double sq = ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
+    return Math.sqrt(sq);
+  }
 }
