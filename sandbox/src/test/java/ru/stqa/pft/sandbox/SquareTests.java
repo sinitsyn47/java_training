43a0a460877d46f4e400aee2a3e3618c57718f85
sinitsyn47 <sk8er47@bk.ru>
2020-10-23 17:24:41 +0300

    Подключение TestNG и тест класса квадрат

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SquareTests {
+
+  @Test
+  public void testArea(){
+    Square r = new Square(7);
+    Assert.assertEquals(r.area(), 49.0);
+  }
+}
