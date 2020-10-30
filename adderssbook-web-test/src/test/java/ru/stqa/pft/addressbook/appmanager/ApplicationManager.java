fade163698a6a4b051d5b22dc4a858682dbf3197
sinitsyn47 <sk8er47@bk.ru>
2020-10-30 17:49:02 +0300

    Реализован двухуровневая архитектура теста
    1. Создан общий базовый класс для тестов
    2. Заменена наследование на делегирование
    3. Выделены специализированные классы-помощники
    4. Создан общий базовый класс для классов-помощников

---
+++ adderssbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -0,0 +1,40 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+  public WebDriver wd;
+
+  private NavigationHelper navigationHelper ;
+  private GroupHelper groupHelper;
+  private SessionHelper sessionHelper;
+
+  public void init() {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+    groupHelper = new GroupHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
+    sessionHelper = new SessionHelper(wd);
+    sessionHelper.login("admin", "secret");
+
+  }
+
+
+  public void stop() {
+    wd.quit();
+  }
+
+
+
+  public GroupHelper getGroupHelper() {
+    return groupHelper;
+  }
+
+  public NavigationHelper getNavigationHelper() {
+    return navigationHelper;
+  }
+}
