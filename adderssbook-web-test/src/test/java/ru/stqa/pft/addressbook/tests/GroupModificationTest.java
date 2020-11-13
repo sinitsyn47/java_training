023caf2c08be18296b0e129ff8fe99c495eb3d4a
sinitsyn47 <sk8er47@bk.ru>
2020-11-13 16:41:36 +0300

    Доработка теста создание группы
    Сделана проверка сравнение списка групп перед созданием и после.

--- adderssbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ adderssbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -28,6 +28,6 @@ public class GroupModificationTest extends TestBase {
 
     before.remove(before.size() - 1);
     before.add(group);
-    Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
