aeb6ce2ffbdb453abc7c420102f8e785af93dcd1
sinitsyn47 <sk8er47@bk.ru>
2020-11-21 02:12:30 +0300

    Создан вспомогательный метод для проверки наличие контакта в таблице при выполнении теста ContactPhoneTests

--- adderssbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTest.java
+++ adderssbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTest.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -11,6 +12,17 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactPhoneTest extends TestBase {
+
+  @BeforeMethod
+  public void ensurePreconditions(){
+    if (app.contact().all().size() == 0){
+      app.contact().create(new ContactData()
+              .withFirstname("Sergey").withLastname("Sinitsyn").withNickname("sergey")
+              .withAddress("Saint-P.").withHomePhone("255-55-99").withMobilePhone("+7(953)3469988").withWorkPhone("8(812)999-55-33")
+              .withEmail("test@mail.ru").withEmail2("work@test.ru").withEmail3("123_test.qwert@gmail.com").withGroup("test1"));
+    }
+    app.goTo().goToHome();
+  }
 
   @Test
   public void testContactPhone(){
