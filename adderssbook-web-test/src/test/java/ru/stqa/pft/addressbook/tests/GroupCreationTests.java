d0a9235de355c32471a4fcd71e883f38329ea76f
sinitsyn47 <sk8er47@bk.ru>
2020-11-27 20:47:43 +0300

    Сохранение тестовых данных в формат json и запуска теста создание групп из файла json

--- adderssbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ adderssbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,8 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -8,7 +10,6 @@ import ru.stqa.pft.addressbook.model.Groups;
 
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,7 +21,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups() throws IOException {
+  public Iterator<Object[]> validGroupsFromXml() throws IOException {
     BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
     String xml = "";
     String line = reader.readLine();
@@ -35,8 +36,23 @@ public class GroupCreationTests extends TestBase {
 
   }
 
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null){
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());  // List<GroupData>.class
+    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
 
-  @Test (dataProvider = "validGroups")
+  }
+
+
+  @Test (dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
