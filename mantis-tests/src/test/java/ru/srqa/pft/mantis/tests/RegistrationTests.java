d9ca80e19daf3046caea6fd9f28ddfaee453b61a
sinitsyn47 <sk8er47@bk.ru>
2020-12-18 18:58:47 +0300

    Ленивая инициализация помощников

---
+++ mantis-tests/src/test/java/ru/srqa/pft/mantis/tests/RegistrationTests.java
@@ -0,0 +1,12 @@
+package ru.srqa.pft.mantis.tests;
+
+import org.testng.annotations.Test;
+
+public class RegistrationTests extends TestBase {
+
+  @Test
+  public void testRegistration(){
+    app.registration().start("user1", "user1@mlocalhost.localdomain");
+
+  }
+}
